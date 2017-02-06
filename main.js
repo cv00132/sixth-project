53f1c5cb0e7a93ea956e1fadabe2f9b908f5826b
Cory Vaughan <codercory13@gmail.com>
2017-02-06 16:04:41 -0500

    Finished by adding a changing color background.

---
+++ main.js
@@ -0,0 +1,33 @@
+var display = document.getElementById("display");
+var bG = document.getElementById("bG");
+
+function clock() {
+  var time = new Date();
+  var hours = time.getHours().toString();
+  var minutes = time.getMinutes().toString();
+  var seconds = time.getSeconds().toString();
+
+   if (hours.length < 2) {
+     hours = "0" + hours;
+   }
+   if (minutes.length < 2) {
+     minutes = "0" + minutes;
+   }
+   if (seconds.length < 2) {
+     seconds = "0" + seconds;
+  }
+
+var timer = hours + ":" + minutes + ":" + seconds;
+var chameleonBG = "#" + hours + minutes + seconds;
+
+display.innerHTML = timer;
+bG.style.backgroundColor = chameleonBG;
+}
+
+setInterval(clock, 1000);
+
+console.log(clock());
+// console.assert(myCallback, 1000)
+
+
+//setInterval(clock(), 1000);
